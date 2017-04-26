76c5cfa749bd079020cc45e75443fb929004ac5f
Nuhman <nuhman7@outlook.com>
2017-04-26 06:51:28 +0530

    update regarding setup of databases

--- static/includes/dbsetup.php
+++ static/includes/dbsetup.php
@@ -1,8 +1,8 @@
 <?php
-define("DB_SERVER","localhost");
-define("DB_USER","root");
-define("DB_PASS","root");
-define("DB_NAME","iqra");
+define("DB_SERVER","localhost"); //replace this with your database server
+define("DB_USER","root"); //replace this with your database username
+define("DB_PASS","root"); //replace this with your database passoword
+define("DB_NAME","iqra"); //replace this with your database name
 
 // Create connection
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
