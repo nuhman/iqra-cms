92d34ede9fb4f17b5eb9ed74d8b17b8c715975fa
Nuhman <nuhman7@outlook.com>
2017-04-26 06:43:35 +0530

    Add files via upload

---
+++ static/includes/layouts/navigation.php
@@ -0,0 +1,61 @@
+<ul class="subjects">
+  <?php
+    $nav_context = " ";
+    if($layout_context === 'public'){
+      $nav_context = "hide";
+    }
+    else{
+      $layout_context = 'admin';
+      $nav_context = "show";
+    }
+
+    $result = get_all_subjects();
+    while($subject = mysqli_fetch_assoc($result)){
+  ?>
+    <li>
+    <?php if($nav_context == "hide"){ ?>
+    <a href="display_all.php?subject=<?php echo urlencode($subject['id'])?>" class="black-text">
+    <?php
+    }
+    else{ ?>
+      <a href="manage_content.php?subject=<?php echo urlencode($subject['id'])?>" class="black-text">
+    <?php } ?>
+
+      <?php
+        echo strtoupper(htmlentities($subject["menu_name"]));
+      ?> </a>
+      <?php
+        $result_1  = get_all_pages_for_sub($subject);
+      ?>
+      <ul class="pages">
+        <?php
+          while($pages = mysqli_fetch_assoc($result_1)){
+          ?>
+          <li
+          <?php
+            if($pages["id"] == $selected_page_id){
+                echo " class='selected pageselected' ";
+            }
+          ?> >
+          <?php if($nav_context == "hide"){ ?>
+              <a href = "display_all.php?page=<?php echo urlencode($pages['id']) ?>" class="gray-text">
+          <?php
+            }
+            else{ ?>
+              <a href = "manage_content.php?page=<?php echo urlencode($pages['id']) ?>" class="gray-text">
+          <?php } ?>
+          <?php echo $pages["menu_item"]; ?></a>
+          </li>		  
+          <?php
+            }
+            mysqli_free_result($result_1);
+          ?>
+
+      </ul>
+    </li>
+	<li><div class="divider"></div></li>
+    <?php
+    }
+    mysqli_free_result($result);
+    ?>
+</ul>
---
+++ static/includes/layouts/open.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>	<title>Iqra<?php
+		if($layout_context === 'public') {
+			echo ' ';
+		}
+		else{ echo '- Admin';
+		}
+		?>
+	</title>
+	<!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
+	<link href="static/styles/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="static/styles/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+	<link href="static/styles/mystyle.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<nav class="white" role="navigation">
+		<div class="nav-wrapper container">
+		  <a id="logo-container" href="index.php" class="brand-logo">iQra<?php
+			if($layout_context === 'public') echo ' ';
+			else{ echo ' - admin'; }
+			?>
+			</a>
+		  <ul class="right hide-on-med-and-down">
+		  <?php if($layout_context === 'public'){ ?>
+			<li><a href="login.php">Log In</a></li>
+			<li><a href="display_all.php">Read</li>
+		  <?php } 
+			else{
+		  ?>		  
+		  <li><a href="admin.php">Menu</a></li>		  
+		  <li><a href="logout.php">Logout</a></li>		  
+			<?php } ?>
+		  </ul>
+
+		  <ul id="nav-mobile" class="side-nav">
+		  <?php if($layout_context === 'public'){ ?>
+			<li><a href="login.php">Log In</a></li>
+			<li><a href="dispaly_all.php">Read</li>
+			<?php } 
+			else{
+		  ?>		  
+		  <li><a href="admin.php">Menu</a></li>		  
+		  <li><a href="logout.php">Logout</a></li>		  
+			<?php } ?>
+		  </ul>
+		  
+		  
+		  <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+		</div>
+		</nav>
+		
+	
+	
